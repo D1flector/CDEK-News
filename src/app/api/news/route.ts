2dd10c2250eb1227df4c2257b189806726a2fb1f
D1flector <dima.kornakow@yandex.ru>
2026-04-10 14:13:36 +0300

    feat: создан API-роут для получения списка новостей

---
+++ src/app/api/news/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+
+  const page = searchParams.get("page") || "1";
+  const perPage = searchParams.get("perPage") || "3";
+  const type = searchParams.get("type") || "company/short";
+
+  const apiUrl = `http://1e14c3489fcb.vps.myjino.ru:5000/api/v1/news/feed/${type}?perPage=${perPage}&page=${page}`;
+
+  try {
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+      return NextResponse.json({ error: "Ошибка API" }, { status: res.status });
+    }
+
+    const data = await res.json();
+
+    return NextResponse.json(data);
+  } catch {
+    return NextResponse.json(
+      { error: "Ошибка соединения с сервером" },
+      { status: 500 }, // чтобы не было бесконечной загрузки
+    );
+  }
+}
